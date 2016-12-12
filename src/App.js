c776c43fdbadffcc5a1c767676d95462256f004d
Alex Xandra Albert Sim <bertzzie@gmail.com>
2016-12-12 10:20:19 +0700

    New Items: events, constructor

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import './App.css';
 
 class App extends Component {
   state = {
-    items: []
+    items: [],
+    newItemText: ""
   };
+
+  constructor (props) {
+    super(props);
+
+    this.addItem = this.addItem.bind(this);
+    this.updateNewItemValue = this.updateNewItemValue.bind(this);
+  }
 
   componentDidMount() {
     this.setState({ items: [
@@ -14,6 +22,20 @@ class App extends Component {
       "???",
       "PROFIT!"
     ] });
+  }
+
+  addItem (event) {
+    if (event.key === "Enter") {
+      const newItems = this.state.items.concat(this.state.newItemText);
+      this.setState({ 
+        items: newItems,
+        newItemText: ""
+      });
+    }
+  }
+
+  updateNewItemValue (event) {
+    this.setState({ newItemText: event.target.value });
   }
 
   render() {
@@ -31,12 +53,19 @@ class App extends Component {
           </header>
 
           <ul id="todo-list">
-            { this.state.items.map(item => <li>{ item }</li>) }
+            { this.state.items.map((item, index) => <li key={`item-${index}`}>{ item }</li>) }
           </ul>
 
           <footer>
             <label htmlFor="new-item">New Item: </label>
-            <input type="text" id="new-item" placeholder="Add Item..." />
+            <input type="text" 
+                   id="new-item"
+                   placeholder="Add Item..."
+                   onChange={this.updateNewItemValue}
+                   onKeyUp={this.addItem}
+                   value={this.state.newItemText} />
+
+            <p>Content is: {this.state.newItemText}</p>
           </footer>
         </content>
       </div>
