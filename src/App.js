cc667ab2b1c4a15f1ea8d136e5a04feb0a93ce0d
Alex Xandra Albert Sim <bertzzie@gmail.com>
2016-12-12 10:03:36 +0700

    UI updates

--- src/App.js
+++ src/App.js
@@ -8,11 +8,27 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Todo List</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <content className="App-content">
+          <header>
+            <label htmlFor="filter">Filter Items: </label>
+            <input type="text" id="filter" placeholder="Filter..." />
+          </header>
+
+          <ul id="todo-list">
+            <li>Buy new mouse and keyboard</li>
+            <li>Call ISP for better internet</li>
+            <li>???</li>
+            <li>PROFIT!</li>
+          </ul>
+
+          <footer>
+            <label htmlFor="new-item">New Item: </label>
+            <input type="text" id="new-item" placeholder="Add Item..." />
+          </footer>
+        </content>
       </div>
     );
   }
